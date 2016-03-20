cf7520d1cf0eac9b71a900ec660af139c0c94069
209255 <209255@student.pwr.edu.pl>
2016-03-20 21:24:30 +0100

    Ignore

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Test.ViewModel;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow:Window
+
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var wizardViewModelMain = new WizardViewModelMain();
+        wizardViewModelMain.RequestClose += OnViewModelRequestClose;
+        this.DataContext = wizardViewModelMain;
+
+    }
+
+
+    private void OnViewModelRequestClose(object sender, EventArgs e)
+    {
+        Close();
+    }
+}
+}
